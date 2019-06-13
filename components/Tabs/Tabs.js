46cf7e58c79ce7b56e6fba1e4b75da19bc5df77d
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-12 22:38:01 -0500

    Getting my first commit in

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
     // this.data;
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    // this.itemElement; 
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
