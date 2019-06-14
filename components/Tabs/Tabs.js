5a74a69093f514a27b3d7363572c24fdf3f68839
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-13 19:48:16 -0500

    confused and needed to read. I got to line 31 and don't quite understand what it's asking for.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,13 +21,15 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll(.tabs-link)
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach( () => {
+      this.links.classList.remove('tabs-links-selected')
+    })
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.
     
     // Call the select method on the item associated with this link
 
